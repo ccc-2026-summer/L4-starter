2d15c80a66330a3312fd5015b51175ce98cacad4
19 <sorarize@gmail.com>
2026-07-10 20:44:49 +0800

    大致完成一個隨機 layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,19 @@
 /* 另一種是可以一次註解多行（符號和 css 一樣）
 快捷鍵是 command option / */
 
+const workItems = document.querySelectorAll('.work-item');
+
+workItems.forEach((item) => {
+  const degree = Math.random() * 360;
+  item.style.transform = `rotate(${degree}deg)`;
+
+  const x = Math.random() * window.innerWidth;
+  const y = Math.random() * window.innerHeight;
+  item.style.left = `${x}px`;
+  item.style.top = `${y}px`;
+
+  item.addEventListener('mouseenter', (event) => {
+    const video = item.querySelector('video');
+    video.play();
+  })
+});
